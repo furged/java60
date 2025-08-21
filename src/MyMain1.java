dfdf2081d63ebdc5a3243fd5562970ace5be56c0
Anushka Shakya <chiyaa2005@gmail.com>
2025-08-21 09:45:52 +0530

    inheritance

--- src/MyMain1.java
+++ src/MyMain1.java
@@ -1,7 +1,7 @@
 public class MyMain1 {
     public static void main(String[] args) {
         CollegeStudents cs=new CollegeStudents("abc", 2);
-        // System.out.println("address " + cs.address + "roll no: " + cs.roll_no);
+        System.out.println("address " + cs.address + "roll no: " + cs.roll_no);
         cs.show_roll();
         cs.show_address();
     }
