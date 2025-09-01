872a1579285198cfa13fce2773241bcccb9f7986
Anushka Shakya <chiyaa2005@gmail.com>
2025-09-01 14:58:28 +0530

    array

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Element at "+ i + " :" + a[i]);
+        }
+    }
+}
