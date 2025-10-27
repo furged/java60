48ce643acf3b73d1a9ffabc5f91c89fd3c596769
Anushka Shakya <chiyaa2005@gmail.com>
2025-10-27 15:30:39 +0530

    triple quotes test

---
+++ src/Unit2/TripleQuotes.java
@@ -0,0 +1,13 @@
+package Unit2;
+
+public class TripleQuotes {
+    public static void main(String[] args) {
+        String quote = "Anushka Shakya";
+        String quote2 = """
+                Anushka Shakya
+                     60""";
+
+        System.out.println(quote);
+        System.out.println(quote2);
+    }
+}
